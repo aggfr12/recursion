99b07ba316cdf2d37f044b0faae163be0e4600ff
Avi Dunn <avidunn@gmail.com>
2014-10-03 11:24:54 -0700

    started elements by class name/ checked if document.body has class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,9 +5,21 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  var child = document.childNodes;
+  
+  var body = document.body;	
   var result = [];
-  var classes = [];
 
-  for(var i=0; i<child.length; i++){}
+  if(body.classList.contains(className)) {
+  	result.push(body);
+  }
+
+  
+  var child = body.childNodes;
+  
+
+  //var child = document.childNodes;
+  //var result = [];
+  //var classes = [];
+
+  //for(var i=0; i<child.length; i++){}
 };
